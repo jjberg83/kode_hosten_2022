bafe32c636e49dfe733b8e51b13166344a5da715
Stealthalgorithm <joakim.polden@gmail.com>
2022-09-09 12:42:15 +0200

    added teststringerbook

---
+++ operativsystemer/lab3/book.go
@@ -0,0 +1,46 @@
+package stringer
+
+import "strconv"
+
+/*
+Task 2: Stringers
+
+One of the most ubiquitous interfaces is Stringer defined by the fmt package.
+
+	type Stringer interface {
+	    String() string
+	}
+
+A Stringer is a type that can describe itself as a string. The fmt package (and
+many others) look for this interface to print values.
+
+Implement the String() method for the Book struct.
+
+# A struct
+
+Book{Title: Practical Engineering, Author: Person{FirstName: John, LastName, Doe}, Distributor: Egmont, ReleaseYear: 2018}
+
+should be printed as
+
+"Title: Practical Engineering by Doe, John. Released: 2018, Egmont Distribution."
+*/
+
+// Person holds the first name and last name of a person.
+type Person struct {
+	FirstName string
+	LastName  string
+}
+
+// Book holds the title, author and other information about a book.
+type Book struct {
+	Title       string
+	Author      Person
+	Distributor string
+	ReleaseYear int
+}
+
+func (b Book) String() string {
+	//s := fmt.Sprintf("Tile: %s by %s, %s. Released: %d, %s.", b.Title, b.Author.LastName, b.Author.FirstName, b.ReleaseYear, b.Distributor)
+	s := ("Title: " + b.Title + " by " + b.Author.LastName + ", " + b.Author.FirstName + ". Released: " + strconv.Itoa(b.ReleaseYear) + ", " + b.Distributor + " Distribution.")
+	return s
+}
